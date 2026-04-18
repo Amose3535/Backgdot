3be1146a7d07e2fb93ffce2a6c191a0f6b88b1aa
Amose Romeo Buzzi <68696888+Amose3535@users.noreply.github.com>
2026-04-18 16:19:51 +0200

    Fixed bug

--- addons/backgroundable/scripts/background_manager.gd
+++ addons/backgroundable/scripts/background_manager.gd
@@ -1,82 +1,90 @@
 extends Node
-## A simple autoloads that adds an easy way to manage if an app can be run in the background or not
 
-## The variable that decides wether the app can be run in the background or not.[br]
-## When [code]backgroundable == true[/code], if the window is closed, the app will keep running in the background.
+## A simple autoload that adds an easy way to manage if an app can be run in the background or not
+
 @export var backgroundable : bool = false:
 	set(new_state):
 		backgroundable = new_state
-		toggle_app_persistenceness_to_quit()
+		# Aggiorniamo lo stato solo se il nodo è già nella scena (evita errori all'avvio)
+		if is_inside_tree():
+			toggle_app_persistenceness_to_quit()
 
-## If the app can run in background, then make the indicator show this tooltip
-@export var indicator_tooltip : String = "":
+@export var indicator_tooltip : String = "App running in background":
 	set(new_tooltip):
-		if new_tooltip != "":
-			indicator_tooltip = new_tooltip
-		if tray_indicator && tray_indicator.is_inside_tree():
+		indicator_tooltip = new_tooltip
+		if tray_indicator:
 			tray_indicator.tooltip = indicator_tooltip
 
-@export var indicator_icon : Resource = load("res://icon.svg"):
+@export var indicator_icon : Resource = preload("res://icon.svg"):
 	set(new_icon):
 		if new_icon is Texture2D:
 			indicator_icon = new_icon
-		if tray_indicator && tray_indicator.is_inside_tree():
+		if tray_indicator:
 			tray_indicator.icon = indicator_icon
 
-var tray_indicator : StatusIndicator = StatusIndicator.new():
-	set(new_indicator):
-		if tray_indicator: # An indicator already exists. Populate it with its own stuff
-			tray_indicator.icon = new_indicator.icon
-			tray_indicator.tooltip = new_indicator.tooltip
+var tray_indicator : StatusIndicator
+var menu : PopupMenu
 
-## The menu used to access the settings
-var menu : PopupMenu = PopupMenu.new()
+func _ready():
+	# Inizializza lo stato corretto appena l'autoload è pronto
+	toggle_app_persistenceness_to_quit()
 
 func toggle_app_persistenceness_to_quit() -> void:
 	if backgroundable:
-		# Works only if NotificationEngine addon is enabled
-		if has_node("/root/NotificationEngine"):
-			get_node("/root/NotificationEngine").notify({"title":"[b][color=red]Attention![/color][/b]","body":"[u]You can now safely close the app as it will stay open in the system tray.","duration":5.0,"animation_duration":2.0})
-		
-		tray_indicator = StatusIndicator.new()
-		add_child(tray_indicator, true)
-		indicator_icon = load("res://icon.svg")
-		indicator_tooltip = "App running in background"
-		menu = PopupMenu.new()
-		add_child(menu, true)
-		menu.add_item("Show app window", 1)
-		menu.add_separator("_____________")
-		menu.add_item("Quit",2)
-		tray_indicator.menu = menu.get_path()
-		menu.id_pressed.connect(_on_menu_button_pressed)
+		# Disattiva la chiusura automatica
 		get_tree().set_auto_accept_quit(false)
-		get_window().close_requested.connect(close_window_but_not_process)
+		
+		# Crea il menu se non esiste
+		if not menu:
+			menu = PopupMenu.new()
+			add_child(menu)
+			menu.add_item("Show app window", 1)
+			menu.add_separator("_____________")
+			menu.add_item("Quit", 2)
+			menu.id_pressed.connect(_on_menu_button_pressed)
+		
+		# Crea l'indicatore se non esiste
+		if not tray_indicator:
+			tray_indicator = StatusIndicator.new()
+			add_child(tray_indicator)
+			tray_indicator.icon = indicator_icon
+			tray_indicator.tooltip = indicator_tooltip
+			tray_indicator.menu = menu.get_path()
+
+		# Notifica opzionale
+		if has_node("/root/NotificationEngine"):
+			get_node("/root/NotificationEngine").notify({"title":"[b][color=red]Attention![/color][/b]","body":"[u]You can now safely close the app as it will stay open in the system tray.[/u]","duration":5.0,"animation_duration":2.0})
+
+		# Connetti la richiesta di chiusura
+		if not get_window().close_requested.is_connected(close_window_but_not_process):
+			get_window().close_requested.connect(close_window_but_not_process)
 
 	else:
-		# Make so that when closing, terminate app
-		get_tree().auto_accept_quit = true # Revert change
+		# Ripristina il comportamento normale
+		get_tree().set_auto_accept_quit(true) 
+		
 		if get_window().close_requested.is_connected(close_window_but_not_process):
 			get_window().close_requested.disconnect(close_window_but_not_process)
-
-func _notification(what):
-	if what == NOTIFICATION_WM_CLOSE_REQUEST:
-		if backgroundable:
-			pass
-		else:
-			get_tree().quit() # default behavior
+		
+		# Pulisci e cancella in sicurezza tray e menu
+		if tray_indicator:
+			tray_indicator.queue_free()
+			tray_indicator = null
+		if menu:
+			menu.queue_free()
+			menu = null
 
 func close_window_but_not_process():
-	get_window().mode = Window.MODE_MINIMIZED
-	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_NO_FOCUS, true)
+	# Nasconde del tutto la finestra dalla barra delle applicazioni
+	get_window().hide()
 
 func _on_menu_button_pressed(id: int) -> void:
 	match id:
 		1:
-			# show window
-			get_tree().get_root().show()
+			# Mostra la finestra
+			get_window().show()
 			get_window().mode = Window.MODE_WINDOWED
-			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_NO_FOCUS, false)
 			get_window().grab_focus()
 		2:
-			#quit_app
+			# Chiudi l'app definitivamente
 			get_tree().quit()
